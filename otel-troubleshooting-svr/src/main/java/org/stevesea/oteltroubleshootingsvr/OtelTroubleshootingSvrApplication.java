e9d747b15934a4fe6c15b5c8fce1d21ea6c77760
steve christensen <schristensen@overstock.com>
2023-03-16 16:57:46 -0600

    initial source from spring boot initializr

---
+++ otel-troubleshooting-svr/src/main/java/org/stevesea/oteltroubleshootingsvr/OtelTroubleshootingSvrApplication.java
@@ -0,0 +1,13 @@
+package org.stevesea.oteltroubleshootingsvr;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OtelTroubleshootingSvrApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OtelTroubleshootingSvrApplication.class, args);
+	}
+
+}
---
+++ otel-troubleshooting-svr/src/test/java/org/stevesea/oteltroubleshootingsvr/OtelTroubleshootingSvrApplicationTests.java
@@ -0,0 +1,13 @@
+package org.stevesea.oteltroubleshootingsvr;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OtelTroubleshootingSvrApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
