a74f45319f94b0eda6b36a74da8c5aca41751eb9
steve christensen <schristensen@overstock.com>
2023-03-16 17:59:45 -0600

    add client lib that uses log4j 1.2

---
+++ clientlib/src/main/java/org/stevesea/oteltroubleshootingclient/Main.java
@@ -0,0 +1,25 @@
+
+package org.stevesea.oteltroubleshootingclient;
+
+import org.apache.log4j.BasicConfigurator;
+import org.apache.log4j.Logger;
+
+public class Main {
+    static Logger logger = Logger.getLogger("org.stevesea.oteltroubleshootingclient");
+
+    public static void main(String[] args) {
+
+        // Set up a simple configuration that logs on the console.
+        BasicConfigurator.configure();
+
+        myFunction();
+    }
+
+
+    public static void myFunction() {
+
+        logger.info("Message from log4j1 client");
+
+
+    }
+}
