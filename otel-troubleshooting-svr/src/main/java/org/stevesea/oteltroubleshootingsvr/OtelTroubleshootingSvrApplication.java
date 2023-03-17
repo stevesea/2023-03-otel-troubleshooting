27efafd0858ee63f8453bdf4ce96024f3fa0871d
steve christensen <schristensen@overstock.com>
2023-03-16 18:03:49 -0600

    update to spring web app, include clientlib artifact

--- otel-troubleshooting-svr/src/main/java/org/stevesea/oteltroubleshootingsvr/OtelTroubleshootingSvrApplication.java
+++ otel-troubleshooting-svr/src/main/java/org/stevesea/oteltroubleshootingsvr/OtelTroubleshootingSvrApplication.java
@@ -1,12 +1,22 @@
 package org.stevesea.oteltroubleshootingsvr;
 
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.stevesea.oteltroubleshootingclient.Main;
 
 @SpringBootApplication
 public class OtelTroubleshootingSvrApplication {
 
+	private static final Logger logger = LogManager.getLogger(OtelTroubleshootingSvrApplication.class);
+
 	public static void main(String[] args) {
+
+		logger.info("in Main");
+
+		Main.myFunction();
+
 		SpringApplication.run(OtelTroubleshootingSvrApplication.class, args);
 	}
 
