ccc5743097219023ae491a58c01194192e1e6ef2
steve christensen <schristensen@overstock.com>
2023-03-16 19:19:09 -0600

    disable unused test

--- otel-troubleshooting-svr/src/test/java/org/stevesea/oteltroubleshootingsvr/OtelTroubleshootingSvrApplicationTests.java
+++ otel-troubleshooting-svr/src/test/java/org/stevesea/oteltroubleshootingsvr/OtelTroubleshootingSvrApplicationTests.java
@@ -1,9 +1,11 @@
 package org.stevesea.oteltroubleshootingsvr;
 
+import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
+@Disabled
 class OtelTroubleshootingSvrApplicationTests {
 
 	@Test
